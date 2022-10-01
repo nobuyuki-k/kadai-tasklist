d552189a7470957ed3d5217d05f8267e8f147f2d
Ubuntu <ubuntu@ip-172-31-44-46.ap-northeast-1.compute.internal>
2022-10-01 01:22:52 +0000

    first commit

---
+++ resources/views/Tasklists/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>Task一覧</h1>
+
+    @if (count($Tasklists) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>Task</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($Tasklists as $task)
+                <tr>
+                    <td>{{ $task->id }}</td>
+                    <td>{{ $task->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+    {{-- ToDo作成ページへのリンク --}}
+    {!! link_to_route('Tasklists.create', '新規ToDoの投稿', [], ['class' => 'btn btn-primary']) !!}
+@endsection
